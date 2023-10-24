1495382e7dc7906eaae986e9791b67463363d128
Mahmudur Rahman Limon <30388998+LIMON100@users.noreply.github.com>
2023-10-24 19:47:26 +0600

    add prediction step

--- Linear-Kalman-Filter/P1/lkf.cpp
+++ Linear-Kalman-Filter/P1/lkf.cpp
@@ -1,36 +1,31 @@
 #include "kalmanfilter.h"
 #include "utils.h"
 
-// -------------------------------------------------- //
-// YOU CAN USE AND MODIFY THESE CONSTANTS HERE
 constexpr bool INIT_ON_FIRST_PREDICTION = true;
 constexpr double INIT_POS_STD = 0;
 constexpr double INIT_VEL_STD = 0;
 constexpr double ACCEL_STD = 0;
 constexpr double GPS_POS_STD = 3.0;
-// -------------------------------------------------- //
 
 void KalmanFilter::predictionStep(double dt)
 {
+    // Initial state and covariance is 0
     if (!isInitialised() && INIT_ON_FIRST_PREDICTION)
     {
-        // Implement the State Vector and Covariance Matrix Initialisation in the
-        // section below if you want to initialise the filter WITHOUT waiting for
-        // the first measurement to occur. Make sure you call the setState() /
-        // setCovariance() functions once you have generated the initial conditions.
-        // Hint: Assume the state vector has the form [X,Y,VX,VY].
-        // Hint: You can use the constants: INIT_POS_STD, INIT_VEL_STD
-        // ----------------------------------------------------------------------- //
-        // ENTER YOUR CODE HERE
-            VectorXd state = Vector4d::Zero();
-            MatrixXd cov = Matrix4d::Zero();
+        VectorXd state = Vector4d::Zero();
+        MatrixXd cov = Matrix4d::Zero();
 
-            // Assume the initial position is (X,Y) = (0,0) m
-            // Assume the initial velocity is 5 m/s at 45 degrees (VX,VY) = (5*cos(45deg),5*sin(45deg)) m/s
+        state<<0, 0, 5.0*cos(M_PI/4), 5.0*sin(M_PI/4);
 
-            setState(state);
-            setCovariance(cov);
-        // ----------------------------------------------------------------------- //
+        const double init_pos_std = INIT_POS_STD;
+        const double init_vel_std = INIT_VEL_STD;
+        cov(0, 0) = INIT_POS_STD * INIT_POS_STD;
+        cov(1, 1) = INIT_POS_STD * INIT_POS_STD;
+        cov(2, 2) = init_vel_std * init_vel_std;
+        cov(3, 3) = init_vel_std * init_vel_std;
+
+        setState(state);
+        setCovariance(cov);
     }
 
     if (isInitialised())
@@ -38,14 +33,22 @@ void KalmanFilter::predictionStep(double dt)
         VectorXd state = getState();
         MatrixXd cov = getCovariance();
 
-        // Implement The Kalman Filter Prediction Step for the system in the  
-        // section below.
-        // Hint: You can use the constants: ACCEL_STD
-        // ----------------------------------------------------------------------- //
-        // ENTER YOUR CODE HERE
+        // Kalman Filter Prediction Step for the system in the  
+        MatrixXd F = Matrix4d();
+        F << 1,0,dt,0,
+             0,1,0,dt,
+             0,0,1,0,
+             0,0,0,1;
 
+        MatrixXd Q = Matrix2d::Zero();
+        Q(0, 0) = (ACCEL_STD * ACCEL_STD);
+        Q(1, 1) = (ACCEL_STD * ACCEL_STD);
 
-        // ----------------------------------------------------------------------- //
+        MatrixXd L = MatrixXd(4, 2);
+        L << (0.5*dt*dt),0,0,(0.5*dt*dt),dt,0,0,dt;
+
+        state = F * state;
+        cov = F * cov * F.transpose() + L * Q * L.transpose();
 
         setState(state);
         setCovariance(cov);
